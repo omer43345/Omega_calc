f83ca0abaa81d5f0c06c1903b92f67d7d1ac2052
omer hadad <omer.hadad1@gmail.com>
2022-12-15 13:01:30 +0200

    add comments to Infix_To_postfix.py python file

--- Infix_To_postfix.py
+++ Infix_To_postfix.py
@@ -1,31 +1,40 @@
 from constants import *
 
 
-def postfix_convertor(token_list: list) -> str:
+def postfix_convertor(token_list: list) -> list:
+    """
+    This function converts the token list infix expression to postfix list expression
+    :param token_list: token list of the infix expression
+    :return: postfix list expression
+    """
     stack = []
-    output = []
+    postfix_list = []
     index = 0
     while index < len(token_list):
         if is_number(token_list[index]):
-            output.append(token_list[index])
+            postfix_list.append(token_list[index])
         elif token_list[index] == '(':
             stack.append('(')
         elif token_list[index] == ')':
             while stack and stack[-1] != '(':
-                output += stack.pop()
+                postfix_list += stack.pop()
             stack.pop()
         else:
             while stack and stack[-1] != '(' and priority_dict[token_list[index]] <= priority_dict[stack[-1]]:
-                output += stack.pop()
+                postfix_list += stack.pop()
             stack.append(token_list[index])
         index += 1
     while stack:
-        output += stack.pop()
-
-    return output
+        postfix_list += stack.pop()
+    return postfix_list
 
 
 def postfixSolver(exp: list) -> int:
+    """
+    This function solves the postfix expression
+    :param exp: postfix expression
+    :return: the result of the expression
+    """
     stack = []
     for character in exp:
         if is_number(character):
@@ -37,7 +46,6 @@ def postfixSolver(exp: list) -> int:
                 stack.append(operators_dict[character](float(val2), float(val1)))
             else:
                 stack.append(operators_dict[character](float(val1)))
-
     return stack.pop()
 
 
